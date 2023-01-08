028da5f335b8b9701a53138eb8291181cfbafeb5
AlenProst <57231780+AlenProst@users.noreply.github.com>
2023-01-08 09:09:19 +0200

    Create 9_create_cloud_users_from_csv.ps1

---
+++ 9_create_cloud_users_from_csv.ps1
@@ -0,0 +1,31 @@
+$my_skuid = (Get-MsolAccountSku).accountskuid
+
+
+for ($counter=0; $counter -lt $my_skuid.Length; $counter++)
+{
+	Write-host $counter,$my_skuid[$counter]
+	
+}
+
+$my_choice = [int](Read-Host "Enter the number of the domain: ")
+
+
+#save the suffix in a variable
+$choice_of_sku = $my_skuid[$my_choice]
+
+$csv =  .{Invoke-WebRequest "https://raw.githubusercontent.com/AlenProst/csv/main/cloud_users_license.csv" -OutFile "C:\UsersTEmp\cloud_users_license.csv" 
+Import-Csv -Path "C:\UsersTEmp\cloud_users_license.csv"}
+
+function create_cloud_users_csv{
+	$domain = "@fr330ff1c3.onmicrosoft.com"
+	$SKU = $choice_of_sku
+	
+	$csv | Select-Object -Property `
+	@{Name="DisplayName";Expression={$_.DisplayName}},
+	@{Name="UserPrincipalName";Expression={$_.UserPrincipalName + $domain}},
+	@{Name="UsageLocation";Expression={$_.UsageLocation}},
+	@{Name="Password";Expression={$_.Password}} | foreach {New-MsolUser -DisplayName $_.DisplayName -UserPrincipalName $_.UserPrincipalName `
+		-UsageLocation $_.UsageLocation -Password $_.Password -LicenseAssignment $SKU} 
+	}
+
+create_cloud_users_csv
