c43b13277cbb4e6dddccfbc55089b7f58303c053
AlenProst <57231780+AlenProst@users.noreply.github.com>
2023-02-25 00:01:03 +0200

    Update 10_Users_in_AD_from-csv.ps1

--- 10_Users_in_AD_from-csv.ps1
+++ 10_Users_in_AD_from-csv.ps1
@@ -9,39 +9,49 @@
 
 
 $ou_name = "syncedO365"
-$ou_path = "DC=f0r3,DC=LOCAL"
+$ou_path = "DC=n4m3,DC=LOCAL"
 $ou_full_path = "OU=$ou_name," + $ou_path
 
 
 New-ADOrganizationalUnit -Name $ou_name -Path $ou_path -ProtectedFromAccidentalDeletion $False
 
-#get the ADForest suffixes in an Powersehll array
-$my_suffix2= Get-adforest | select UPNSuffixes -ExpandProperty UPNSuffixes
+# Get the ADForest suffixes in a PowerShell array
+$my_suffixes = Get-ADForest | Select-Object -ExpandProperty UPNSuffixes
 
-#looping throug the array with the index of the suffix
-
-for ($counter=0; $counter -lt $my_suffix2.Length; $counter++){Write-host $counter,$my_suffix2[$counter]}
-
-#read the input from user 
-$my_choice = [int](Read-Host "Enter the number of the domain: ")
-
-
-#save the suffix in a variable
-$suffix = "@" + $my_suffix2[$my_choice]
-
-#creating the password for the users
-$pwd1 = ConvertTo-SecureString "1" -AsPlainText -Force
-
-Import-Csv -Path "C:\Users\Administrator\Desktop\local_AD.csv"| Select-Object -Property `
-@{Name="UserPrincipalName";Expression={$_.UserPrincipalName + $suffix}},
-@{Name="Name";Expression={$_.DisplayName}},
-@{Name="Country";Expression={$_.UsageLocation}} | foreach{New-ADUser -Name $_.Name `
--AccountPassword $pwd1 `
--UserPrincipalName $_.UserPrincipalName `
--Enabled $True `
--Country $_.Country `
--Path $ou_full_path
+# If there is only one suffix, use it directly without looping
+if ($my_suffixes.Count -eq 1) {
+    $suffix = "@" + $my_suffixes[0]
+} else {
+    # Loop through the array with the index of the suffix
+    for ($counter=0; $counter -lt $my_suffixes.Length; $counter++){
+        Write-host $counter, $my_suffixes[$counter]
+    }
+    # Read the input from user 
+    $my_choice = [int](Read-Host "Enter the number of the domain: ")
+    # Save the suffix in a variable
+    $suffix = "@" + $my_suffixes[$my_choice]
 }
 
+# Creating the password for the users
+$pwd1 = ConvertTo-SecureString "1" -AsPlainText -Force
 
-#Remove-ADOrganizationalUnit -Identity "OU=syncedO365,DC=f0r3,DC=LOCAL" -Recursive -Confirm:$False
+# Import the user data from CSV file
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/AlenProst/csv/main/local_ad.csv -OutFile 'C:\Users\Administrator\Desktop\local_ad.csv'
+
+# Loop through each user in the CSV file and create a new AD user
+Import-Csv -Path "C:\Users\Administrator\Desktop\local_ad.csv" | 
+    Select-Object -Property @{
+        Name="UserPrincipalName";Expression={$_.UserPrincipalName + $suffix}
+    }, @{
+        Name="Name";Expression={$_.DisplayName}
+    }, @{
+        Name="Country";Expression={$_.UsageLocation}
+    } | 
+    foreach {
+        New-ADUser -Name $_.Name `
+        -AccountPassword $pwd1 `
+        -UserPrincipalName $_.UserPrincipalName `
+        -Enabled $True `
+        -Country $_.Country `
+        -Path $ou_full_path
+    }
